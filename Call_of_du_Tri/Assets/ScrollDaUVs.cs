c3c76ac17406f8f57d73a2baf5b862fde95ff577
Cosmografik <bretzel75@gmail.com>
2018-07-18 14:38:51 +0200

    Creating a Level Design

---
+++ Call_of_du_Tri/Assets/ScrollDaUVs.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollDaUVs : MonoBehaviour {
+
+    public Vector2 scrollSpeed;
+    Renderer renderer;
+    Material material;
+
+
+	// Use this for initialization
+	void Start () {
+        renderer = GetComponent<Renderer>();
+        material = renderer.material;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        material.SetTextureOffset("_MainTex", scrollSpeed * Time.time);
+        renderer.material = material;
+	}
+}
